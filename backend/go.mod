bee238546a078d99eeeaff87dfecf91d9edc69a8
SmauelL <ljxlm502661058@163.com>
2020-01-20 14:28:50 +0800

    finish

---
+++ backend/go.mod
@@ -0,0 +1,5 @@
+module github.com/SmauelL/realtime-chat-go-react
+
+go 1.12
+
+require github.com/gorilla/websocket v1.4.1
---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Header.scss";
+
+const Header = () => (
+    <div className="header">
+        <h2>在线聊天室</h2>
+    </div>
+);
+
+export default Header;
